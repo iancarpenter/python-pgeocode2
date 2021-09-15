ae49b848c7e5a9f9dc28252688cf8a29d39bbdea
iancarpenter <ianhcarpenter@gmail.com>
2021-09-15 22:07:14 +0100

    Added save functionality

--- postcode_distance/postcode/views.py
+++ postcode_distance/postcode/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render
 from .forms import PostcodeForm
+from .models import Postcode
 import pgeocode
 
 
 def postcode(request):
+
     if request.method == "POST":
+
         form = PostcodeForm(request.POST)
+
         if form.is_valid():
-            #form.save()
+            # with model forms you can call form.save which works because it has a model associated with it
+            # and knows what to save and where
+            # This isn't available here so get a handle to the form valid data 
+            cd = form.cleaned_data
             
+            # Postcode is a model that can be found in the models.py file. 
+            # Create an object, populating the attributes with the values from the form
+            pc = Postcode(
+                start_postcode = cd['start_postcode'],
+                end_postcode = cd['end_postcode'],
+                result_measurement_unit = cd['distance_unit']
+            )
+            # can finally save
+            pc.save()
+
             start_postcode = form['start_postcode'].value()
             end_postcode = form['end_postcode'].value()
 
